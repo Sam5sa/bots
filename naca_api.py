92000e4a42442a81e20484fb01d288605a2fd7db
Sam5sa <G_sam03@mail.ru>
2022-10-21 16:21:32 +0400

    g

--- naca_api.py
+++ naca_api.py
@@ -2,23 +2,16 @@ import requests
 import datetime
 from random import randint
 import time
+import configparser
 
-'''
-endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
-api_key = "Rb39uvdNUsLM36vrcoRWmTTe0bqsd6LLqR9JgyV5"
-query_params = {"api_key": api_key, "earth_date": "2020-10-21"}
-response = requests.get(endpoint, params=query_params)
-#print(response.json())
+config = configparser.ConfigParser()
+config.read("config.ini")
+api_key = config["naca_api"]["key"]
 
-photos = response.json()["photos"]
-print(f"Найдено {len(photos)} фотографий.")
 
-print(photos[1]["img_src"])
-'''
 
 def send_pic_url():
     endpoint = "https://api.nasa.gov/planetary/apod"
-    api_key = "Rb39uvdNUsLM36vrcoRWmTTe0bqsd6LLqR9JgyV5"
     d = datetime.date.today()
     str_yesterday = str(d.year)+"-"+str(d.month)+"-"+str(d.day-1)
     query_params = {"api_key": api_key, "date": str_yesterday}
@@ -27,7 +20,6 @@ def send_pic_url():
 
 def send_explanation():
     endpoint = "https://api.nasa.gov/planetary/apod"
-    api_key = "Rb39uvdNUsLM36vrcoRWmTTe0bqsd6LLqR9JgyV5"
     d = datetime.date.today()
     str_yesterday = str(d.year)+"-"+str(d.month)+"-"+str(d.day-1)
     query_params = {"api_key": api_key, "date": str_yesterday}
@@ -36,7 +28,6 @@ def send_explanation():
 
 def send_rand_pic():
     endpoint = "https://api.nasa.gov/planetary/apod"
-    api_key = "Rb39uvdNUsLM36vrcoRWmTTe0bqsd6LLqR9JgyV5"
     flag  = 0
     while (flag == 0) :
         year = randint(2019,2022)
