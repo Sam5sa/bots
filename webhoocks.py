5da5b77b6b8beb3e506a1e6168dde60c82f04d37
Sam5sa <G_sam03@mail.ru>
2022-10-22 20:29:46 +0400

    google sheets integration

--- webhoocks.py
+++
@@ -1,8 +0,0 @@
-import telebot
-import configparser
-from telebot import types
-
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-bot = telebot.TeleBot(config["pyrogram"]["api_token"])
